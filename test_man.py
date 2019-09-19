b1542249f18a8636783a5eae8be30595023f7183
Luo Man <luoman.cs@gmail.com>
2019-09-19 13:09:53 -0700

    user program for digit example

--- test_man.py
+++ test_man.py
@@ -1,6 +1,7 @@
-from dlpmln import deeplpmln
+from dlpmln import DeepLPMLN
 from digit_network import Net, device
 import torch
+import numpy as np
 # import torch.nn.functional as F
 # import torch.optim as optim
 from torchvision import datasets, transforms
@@ -18,32 +19,28 @@ nn(m(X,1), digit, [0,1,2,3,4,5,6,7,8,9]) :- img(X).
 # data = [{'i1': }]
 use_cuda = False
 
-# kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+train_loader = torch.utils.data.DataLoader(
+	datasets.MNIST('../data', train=True, download=True,
+				   transform=transforms.Compose([
+					   transforms.ToTensor(),
+					   transforms.Normalize((0.1307,), (0.3081,))
+				   ])),
+	batch_size=2, shuffle=True, **kwargs)
 
-# train_loader = torch.utils.data.DataLoader(
-# 	datasets.MNIST('../data', train=True, download=True,
-# 				   transform=transforms.Compose([
-# 					   transforms.ToTensor(),
-# 					   transforms.Normalize((0.1307,), (0.3081,))
-# 				   ])),
-# 	batch_size=2, shuffle=True, **kwargs)
-
-# test_loader = torch.utils.data.DataLoader(
-# 	datasets.MNIST('../data', train=False, transform=transforms.Compose([
-# 					   transforms.ToTensor(),
-# 					   transforms.Normalize((0.1307,), (0.3081,))
-# 				   ])),
-# 	batch_size=1000, shuffle=True, **kwargs)
+test_loader = torch.utils.data.DataLoader(
+	datasets.MNIST('../data', train=False, transform=transforms.Compose([
+					   transforms.ToTensor(),
+					   transforms.Normalize((0.1307,), (0.3081,))
+				   ])),
+	batch_size=1000, shuffle=True, **kwargs)
 
 m = Net().to(device)
 
-# is this __name__ necessary?
+nnDic = {"m":m}
 
-nn_dic ={"m":m}
 
-optimizer = torch.optim.Adam(m.parameters(), lr=0.001)
-
-dlpmlnObj = deeplpmln(dprogram, nn_dic, [optimizer])
+optimizer = {'m':torch.optim.Adam(m.parameters(), lr=0.001)}
 
 obs = [""]
 
@@ -52,13 +49,28 @@ data_m = datasets.MNIST('../data', train=True, download=True,
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ]))
-# data_m = torch.Tensor(data_m)
 
-# print(data_m.size())
+dataList = []
+obstxt = ""
+obsList = []
+for dataIdx, data in enumerate(train_loader):
+	# print("This is data 1:",data[0][0])
+	# print("This is data 2:", data[0][1])
+	# print(data[0].shape)
+	# sys.exit()
+	dataList.append({"m":{"i1":data[0][0].view(1, 1, 28, 28), "i2":data[0][1].view(1, 1, 28, 28)}})
+	obsList.append("addition(i1, i2, {}).".format(data[1][0]+data[1][1]))
+	obstxt += "addition(i1, i2, {}).\n".format(data[1][0]+data[1][1])
+	obstxt += "#evidence\n"
+	if dataIdx > 10:
+		break
+with open("evidence.txt", "w") as f:
+	f.write(obstxt)
 
-data = {'m':train_loader}
 
-dlpmlnObj.learn1(data, obs, optimizer, 1)
+dlpmlnObj = DeepLPMLN(dprogram, nnDic, optimizer)
+
+dlpmlnObj.learn1(dataList, obsList, 1)
 
 
 # print(dlpmlnObj.mvpp)
